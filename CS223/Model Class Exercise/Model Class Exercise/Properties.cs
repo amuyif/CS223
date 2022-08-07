5f975324e40f0adb969792e315e10abd5a8c1014
amuyif <100996038+amuyif@users.noreply.github.com>
2022-08-07 07:03:15 +0300

    Add files via upload

---
+++ CS223/Model Class Exercise/Model Class Exercise/Properties.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms ;
+
+namespace Model_Class_Exercise
+{
+    internal class Propertiess
+    {
+        static List<Propertiess> props = new List<Propertiess>();
+        public int itemId { get; set; }
+        public string datee { get; set; }
+        public int inventnum { get; set; }
+        public string objectName{ get; set; }
+
+        public int count { get; set; }
+        public int price { get; set; }
+
+        public bool isAvailable { get; set; }   
+
+        public void save()
+        {
+            props.Add(this);
+        }
+        
+        public static List<Propertiess> GetPropertiesses()
+        {
+            return props;
+        }
+    }
+}
---
+++ CS223/Model Class Exercise/Model Class Exercise/login.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Model_Class_Exercise
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+            lbl_maintxt.Parent = pictureBox1;
+            lbl_maintxt.BackColor = Color.Transparent;
+            lbl_user.Parent = pictureBox1;
+            lbl_user.BackColor = Color.Transparent;
+            lbl_pass.Parent= pictureBox1;
+            lbl_pass.BackColor = Color.Transparent;
+            this.AcceptButton = btn_login;
+            this.txt_password.UseSystemPasswordChar = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username=txt_user.Text;
+            string password = txt_password.Text;
+            if (username != "admin" || password != "admin")
+            {
+                MessageBox.Show("Wrong Username or Password");
+            }
+            else
+            {
+                Form1 screen = new Form1();
+                this.Hide();
+                screen.Show();
+            }
+
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
