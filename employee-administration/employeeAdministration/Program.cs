b470515ce1846bb6f2eea55b349a094cf115478c
Amanuel <amuyif@gmail.com>
2022-08-12 18:04:27 +0300

    admin

---
+++ employee-administration/employeeAdministration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Data;
+
+namespace employeeAdministration
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            dataBase DB = new dataBase();
+
+            DataSet ds = DB.readData("SELECT * FROM users;");
+
+            // DataGrid dg = new DataGrid(); 
+
+            //tools tools = new tools();
+
+            //tools.checkValue("username","admin");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new administration());
+        }
+    }
+}
---
+++ employee-administration/employeeAdministration/database.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace employeeAdministration
+{
+    class dataBase
+    {
+        protected string server = "127.0.0.1";
+        protected string userid = "employeeadmin";
+        protected string password = "employeeadmin";
+        protected string database = "employeeadmin";
+
+        MySqlConnection conn = null;
+        DataSet ds = new DataSet();
+
+        public DataSet readData(String sql){
+
+            try
+            {
+                MySqlConnection conn = this.connectToDB();
+                
+                MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
+                da.Fill(ds);
+
+                
+                MessageBox.Show("MSG: reading data ", "database read success", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                this.disconnectFromDB(conn);
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.ToString(), "database read error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            return ds;
+        }
+
+        private MySqlConnection connectToDB()
+        {
+            string cs = @"server=" + this.server + ";userid=" + this.userid + ";password=" + this.password + ";database=" + this.database + "";
+
+
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+
+                //debug
+                //MessageBox.Show("MySQL version :" + conn.ServerVersion.ToString() + "info :" + conn.Database.ToString() + "", "server info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.ToString(), "database connection error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+            return conn;
+
+        }
+        private void disconnectFromDB(MySqlConnection conn)
+        {
+
+            try
+            {
+                //debug
+                //MessageBox.Show("Colsed conn" + conn.ServerVersion.ToString() + "info" + conn.Database.ToString() + "", "server info", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error:" + ex.ToString(), "database connection error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+        }
+    }
+}
